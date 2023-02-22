4c10e0ee32272d93a4cb5caed7632743ce40c3b7
Nick Johnson <nlj222@gmail.com>
2023-02-21 16:05:04 -0800

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    if length > 0:
+        sequence.append(0)
+        if length > 1:
+            sequence.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    sequence.append(sequence[i-1] + sequence[i-2])
+    print(sequence)
+
+print_fibonacci(20)
